ea3df3274fbd37ac99f3344c7a4bf8898a662ca8
Nirmal koswatta <nirmalkoza@gmail.com>
2025-07-15 15:40:20 +0530

    Initial project commit

--- src/pages/AuthPages/Login/Login.jsx
+++ src/pages/AuthPages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoginForm from '../../../components/AuthComponents/LoginForm';
 import { Link } from 'react-router-dom';
-import backgroundImage from '../../../assets/login.jpg'; 
+import backgroundImage from '../../../assets/7620.jpg'; 
 import '../../../components/AuthComponents/ModernAuth.css';
 
 const Login = () => {
@@ -15,15 +15,27 @@ const Login = () => {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'relative',
+    overflow: 'hidden',
   };
 
   return (
     <div style={containerStyle}> 
-      <h1 className="modern-heading display-1 my-5 text-center">Login Here</h1>
-      <div className="row justify-content-center w-100">
-        <div className="col-md-6 modern-card p-4" style={{marginTop: '2rem'}}>
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(255,255,255,0.25)',
+        zIndex: 0,
+        animation: 'fadeIn 1.2s',
+      }} />
+      <h1 className="modern-heading display-1 my-5 text-center fade-in" style={{zIndex: 1}}>Login Here</h1>
+      <div className="row justify-content-center w-100" style={{zIndex: 1}}>
+        <div className="col-md-6 modern-card p-4 animated-modal" style={{marginTop: '2rem'}}>
           <LoginForm />
-          <Link to="/register" className="d-block text-center mt-3 auth-link">
+          <Link to="/register" className="d-block text-center mt-3 auth-link fade-in">
             Not a member? Register
           </Link>
         </div>
